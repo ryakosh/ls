01a8e19f57ee3201eb9dfb690c5b182591e9a7b1
ryakosh <ryakosh@gmail.com>
2020-04-17 17:31:54 +0430

    Only support errors that we are aware of

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,4 @@ pub enum Error {
     NF(#[fail(cause)] io::Error, PathBuf),
     #[fail(display = "ls: cannot open directory '{:?}': Permission denied", _1)]
     PD(#[fail(cause)] io::Error, PathBuf),
-    #[fail(display = "error: an io error has accoured '{:?}':\n\n{:?}", _1, _0)]
-    IO(#[fail(cause)] io::Error, PathBuf),
 }
