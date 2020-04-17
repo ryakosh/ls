46fa286e7e4da5669c5515006ff68966f0401f69
ryakosh <ryakosh@gmail.com>
2020-04-17 17:31:19 +0430

    Optimize get_contents() by checking all once

--- src/lib.rs
+++ src/lib.rs
@@ -12,21 +12,21 @@ use {
 
 pub type Contents = Vec<PathBuf>;
 
-pub async fn get_contents(path: &Path, all: bool) -> Result<Contents, Error> {
+pub async fn get_contents(path: &Path, all: bool) -> Result<Contents, failure::Error> {
     match read_dir(path).await {
         Ok(mut stream) => {
             let mut contents = vec![];
-            while let Some(dir_entry) = stream.next().await {
-                let dir_entry = dir_entry.map_err(|e| {
-                    Error::IO(e, path.to_owned())
-                });
 
-                let dir_entry = dir_entry?;
+            if all {
+                while let Some(dir_entry) = stream.next().await {
+                    let dir_entry = dir_entry?;
 
-                if all {
                     contents.push(dir_entry.path());
+                }
+            } else {
+                while let Some(dir_entry) = stream.next().await {
+                    let dir_entry = dir_entry?;
 
-                } else {
                     if !dir_entry.file_name().to_str().unwrap().starts_with('.') {
                         contents.push(dir_entry.path());
                     }
@@ -39,12 +39,12 @@ pub async fn get_contents(path: &Path, all: bool) -> Result<Contents, Error> {
         Err(e) => {
             let path = path.to_owned();
             match e.kind() {
-                ErrorKind::NotFound => Err(Error::NF(e, path)),
-                ErrorKind::PermissionDenied => Err(Error::PD(e, path)),
+                ErrorKind::NotFound => Err(Error::NF(e, path).into()),
+                ErrorKind::PermissionDenied => Err(Error::PD(e, path).into()),
                 ErrorKind::Other if e.to_string().starts_with("Not a directory") => {
                     Ok(vec![path])
                 }
-                _ => Err(Error::IO(e, path))
+                _ => Err(e.into())
             }
         }
     }
